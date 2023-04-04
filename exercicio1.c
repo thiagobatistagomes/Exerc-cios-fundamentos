86ba630a735e564fe0a37f36fe794e28768cd55a
Thiago <122994560+thiagobatistagomes@users.noreply.github.com>
2023-04-04 13:39:44 -0300

    Initial commit

---
+++ exercicio1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	printf("Por mim se vai das dores a morada, \n");
+	printf("Por mim se vai ao padecer eterno, \n");
+	printf("Por mim se vai a gente condenada. \n");
+	printf("\n");
+	printf("Moveu Justica o Autor meu sempiterno, \n");
+	printf("Formado fui por divinal possanca, \n");
+	printf("Sabedoria suma e amor supremo. \n");
+	printf("\n");
+	printf("No existir, ser nenhum a mim se avança, \n");
+	printf("Não sendo eterno, e eu eternal perduro: \n");
+	printf("Deixai, ó vós que entrais, toda a esperança! \n");
+	printf("\n");
+	printf("Estas palavras, em letreiro escuro, \n");
+	printf("Eu vi, por cima de uma porta escrito. \n");
+	printf("“Seu sentido” — disse eu — “Mestre me é duro” \n");
+	return 0;
+}
---
+++ exercicio2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	printf("\tAs parelhas das carruagens do Farao \n");
+	printf("eu te comparo, minha amada. \n");
+	printf("\n");
+	printf("\tGraciosas são tuas faces entre os brincos, \n");
+	printf("e teu pescoço entre colares. \n");
+	printf("\n");
+	printf("\tFaremos para ti brincos de ouro \n");
+	printf("com filigranas de prata. \n");
+	return 0;
+}
